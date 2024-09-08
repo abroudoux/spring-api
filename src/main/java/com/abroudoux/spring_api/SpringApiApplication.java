51a1a1bb23b0422d96a648e236b8891593f74166
arthur <arthur.broudoux@gmail.com>
2024-09-08 23:01:18 +0200

    feat: create GET method Greeting Signed-off-by: Arthur Broudoux <arthur.broudoux@gmail.com>

--- src/main/java/com/abroudoux/spring_api/SpringApiApplication.java
+++ src/main/java/com/abroudoux/spring_api/SpringApiApplication.java
@@ -2,8 +2,9 @@ package com.abroudoux.spring_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude =  {DataSourceAutoConfiguration.class })
 public class SpringApiApplication {
 
 	public static void main(String[] args) {
